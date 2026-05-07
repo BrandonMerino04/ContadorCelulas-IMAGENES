ffc6ea793f4635049dd0eee16c50c23bdc10f7e1
BrandonMerino <brandonpro164@gmail.com>
2026-05-07 08:32:09 -0600

    cambios

--- descarga.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="contador", packages=find_packages(), install_requires=[
-        "numpy",
-        "opencv-python",
-        "matplotlib"
-    ],
-)
